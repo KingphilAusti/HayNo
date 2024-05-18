0ae62bb836924857f388ec054fb6fcc6905738ff
René Philipp Austermühl <austermuehl.rene@gmail.com>
2024-05-18 15:26:33 +0200

    Stream works now! HYPE

--- hayno/src/Chatbot.js
+++ hayno/src/Chatbot.js
@@ -5,9 +5,10 @@ import ChatLog from './scripts/chatLog';
 function Chatbot() {
     const [message, setMessage] = useState('');
     const [chatLog, setChatLog] = useState((chatLog) => new ChatLog(chatLog));
+    const [updateCall, setUpdateCall] = useState('');
 
     
-    const askMessage = async () => {
+    const askMessage = () => {
         try {
             const newChatLog = new ChatLog(chatLog.log);
             newChatLog.add('user', message);
@@ -19,21 +20,23 @@ function Chatbot() {
     };
 
     useEffect( () => {    
-        const processOpenAiStream = (stream) => {
-            var answer = "";
+        const processOpenAiStream = async (stream) => {
+            // setAnswer('');
             const newChatLog = new ChatLog(chatLog.log);
+            var currentAnswert = '';
             if (stream.iterator) {
                 newChatLog.add('assistant', 'Processing...');
-                for (const part of stream) {
-                    answer += part.choices[0]?.delta?.content || '';
-                    newChatLog.updateLastEntry(answer);
+                for await (const part of stream) {
+                    currentAnswert += part.choices[0]?.delta?.content || '';
+                    setUpdateCall(currentAnswert);
+                    newChatLog.updateLastEntry(currentAnswert);
                     console.log(part.choices[0]?.delta?.content || '');
                     setChatLog(newChatLog);
                 }
             } else {
                 console.error('Stream is not iterable');
             }
-            return answer;
+            return updateCall;
         };
 
         if (chatLog.first() && chatLog.first().role === 'user') try {
