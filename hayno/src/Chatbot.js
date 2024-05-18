c32039b0fb16dd73ebca43291598c633e02e2777
René Philipp Austermühl <austermuehl.rene@gmail.com>
2024-05-18 16:53:39 +0200

    Chat now works!

--- hayno/src/Chatbot.js
+++ hayno/src/Chatbot.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import {processMessage} from './scripts/answer';
 import ChatLog from './scripts/chatLog';
+import './App.css';
 
 function Chatbot() {
     const [message, setMessage] = useState('');
     const [chatLog, setChatLog] = useState((chatLog) => new ChatLog(chatLog));
     const [updateCall, setUpdateCall] = useState('');
 
-    
+
     const askMessage = () => {
         try {
             const newChatLog = new ChatLog(chatLog.log);
@@ -19,7 +20,18 @@ function Chatbot() {
         }
     };
 
-    useEffect( () => {    
+    const alignMessage = (role) => {
+        switch (role) {
+            case 'user':
+                return 'chatBubble__user';
+            case 'assistant':
+                return 'chatBubble__assistant';
+            default:
+                return '';
+        }
+    }
+
+    useEffect( () => {
         const processOpenAiStream = async (stream) => {
             // setAnswer('');
             const newChatLog = new ChatLog(chatLog.log);
@@ -41,25 +53,27 @@ function Chatbot() {
 
         if (chatLog.first() && chatLog.first().role === 'user') try {
             processMessage(chatLog).then((response) => {processOpenAiStream(response);})
-        }  catch (error) {  
+        }  catch (error) {
             console.error(error);
         }
-    }, [chatLog]);
+    }, [chatLog, updateCall]);
 
     return (
         <div>
             <div>
-                <div id="chat">
+                <div id="chat" className='chatContainer'>
                     {chatLog.log.map((msg, index) => (
-                        <div key={index} className="message">
+                        <p key={index} className={alignMessage(msg.role)}>
                             <strong>{msg.role}:</strong> {msg.content}
-                        </div>
+                        </p>
                     ))}
                 </div>
-                <form onSubmit={(e) => { e.preventDefault(); askMessage(); }}>
-                    <input type="text" value={message} onChange={(e) => setMessage(e.target.value)} placeholder="Send a message" />
-                    <input type="submit" value="Send" />
-                </form>
+                <div>
+                    <form className='embedSubmitField' onSubmit={(e) => { e.preventDefault(); askMessage(); }}>
+                        <input className='inputTextField' type="text" value={message} onChange={(e) => setMessage(e.target.value)} placeholder="Send a message" />
+                        <button type="submit" value="Send">Send</button>
+                    </form>
+                </div>
             </div>
         </div>
     );
